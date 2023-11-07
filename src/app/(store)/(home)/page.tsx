55b0d3f3a03f836fbc1fdde1b5320a2dd9dcd0e6
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2023-11-07 12:00:24 -0300

    feat: first api integration

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,48 +1,69 @@
+import { api } from '@/data/api'
+import { Product } from '@/data/types/products'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Home() {
+async function getFeaturedProducts(): Promise<Product[]> {
+  const response = await api('/products/featured')
+
+  const products = await response.json()
+
+  return products
+}
+
+export default async function Home() {
+  const [highLightedProduct, ...otherProducts] = await getFeaturedProducts()
   return (
     <div className="grid max-h-[920px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href="/"
-        className="group col-span-6 row-span-6 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+        href={`/product/${highLightedProduct.slug}`}
+        className="group relative col-span-6 row-span-6 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
       >
         <Image
           className="transition-transform duration-500 ease-in-out group-hover:scale-105"
-          src="/moletom-ai-side.png"
+          src={highLightedProduct.image}
           width={920}
           height={920}
           quality={90}
-          alt="moletom"
+          alt={highLightedProduct.title}
         />
+        <div className="absolute bottom-28 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">{highLightedProduct.title}</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            {highLightedProduct.price.toLocaleString('pt-br', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
+          </span>
+        </div>
       </Link>
-      <Link
-        href="/"
-        className="group col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
-      >
-        <Image
-          className="transition-transform duration-500 ease-in-out group-hover:scale-105"
-          src="/camiseta-dowhile-2022.png"
-          width={920}
-          height={920}
-          quality={90}
-          alt="moletom"
-        />
-      </Link>
-      <Link
-        href="/"
-        className="group col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
-      >
-        <Image
-          className="transition-transform duration-500 ease-in-out group-hover:scale-105"
-          src="/moletom-ai-side.png"
-          width={920}
-          height={920}
-          quality={90}
-          alt="moletom"
-        />
-      </Link>
+      {otherProducts.map((product) => {
+        return (
+          <Link
+            key={product.id}
+            href={`/product/${product.slug}`}
+            className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+          >
+            <Image
+              className="transition-transform duration-500 ease-in-out group-hover:scale-105"
+              src={product.image}
+              width={920}
+              height={920}
+              quality={90}
+              alt={product.title}
+            />
+            <div className="absolute bottom-10 right-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <span className="truncate text-sm">{product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {product.price.toLocaleString('pt-br', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })}
+              </span>
+            </div>
+          </Link>
+        )
+      })}
     </div>
   )
 }
